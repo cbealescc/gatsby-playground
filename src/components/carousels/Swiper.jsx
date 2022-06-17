7afada612a5bc9a883c149c15479a469286c3397
Conor Beales <connor.beales@collectingcars.com>
2022-06-17 12:22:25 +0100

    Dynamic content added and styling started

--- src/components/carousels/Swiper.jsx
+++ src/components/carousels/Swiper.jsx
@@ -1,10 +1,10 @@
-import React, {useState, useEffect} from 'react';
+import React from 'react';
 
 // Import Swiper React components
 import {Swiper, SwiperSlide} from 'swiper/react';
 
 // import Swiper core and required modules
-import {Pagination} from 'swiper';
+import {Pagination, Autoplay} from 'swiper';
 
 // Import Swiper styles
 import 'swiper/css';
@@ -19,29 +19,45 @@ const Slides = () => {
             type: 'image',
             imgUltraWide: `${baseUrl}012042/IMG-3569.jpg`,
             imgDesktop: `${baseUrl}012042/30-ww-16.jpg`,
-            imgMobile: `${baseUrl}012042/IMG-3590.jpg`
+            imgMobile: `${baseUrl}012042/IMG-3590.jpg`,
+            title: 'Supercar Sunday!',
+            previewText: `If you're a Supercar fan, you'll love this!`,
+            link: '/collection/supercar-sunday',
+            buttonText: 'View Collection'
         },
         {
             type: 'image',
             imgUltraWide: `${baseUrl}012080/MMP-49.jpg`,
             imgDesktop: `${baseUrl}012080/MMP-1.jpg`,
-            imgMobile: `${baseUrl}012080/MMP-66.jpg`
+            imgMobile: `${baseUrl}012080/MMP-66.jpg`,
+            title: 'Air-Cooled Sunday!',
+            previewText: `Over 200 air-cooled Porsches.`,
+            link: '/collection/air-cooled-sunday',
+            buttonText: 'View Collection'
         },
+        {
+            type: 'video',
+            videoUrlMP4: `https://ak.picdn.net/shutterstock/videos/1062622186/preview/stock-footage-aerial-shot-of-electrical-car-driving-on-country-road-at-summer-evening-new-suv-vehicle-moving.mp4`,
+            videoUrlWEBM: `https://ak.picdn.net/shutterstock/videos/1062622186/preview/stock-footage-aerial-shot-of-electrical-car-driving-on-country-road-at-summer-evening-new-suv-vehicle-moving.webm`,
+            title: `Chris Harris' Column`,
+            previewText: `"Working hard and wasting money on machinery you simply do not need is the very essence of loving cars".`,
+            link: '/stories/chris-harris-column-week-2',
+            buttonText: 'Read More'
+        }
         // {
         //     type: 'video',
         //     videoUrlMP4: `https://images-collectingcars-com.s3.eu-west-2.amazonaws.com/editorial/videos/cc-best-offer-explained.mp4`,
         //     videoUrlWEBM: `https://images-collectingcars-com.s3.eu-west-2.amazonaws.com/editorial/videos/cc-best-offer-explained.mp4`
         // },
-        {
-            type: 'video',
-            videoUrlMP4: `https://ak.picdn.net/shutterstock/videos/1062622186/preview/stock-footage-aerial-shot-of-electrical-car-driving-on-country-road-at-summer-evening-new-suv-vehicle-moving.mp4`,
-            videoUrlWEBM: `https://ak.picdn.net/shutterstock/videos/1062622186/preview/stock-footage-aerial-shot-of-electrical-car-driving-on-country-road-at-summer-evening-new-suv-vehicle-moving.webm`
-        }
     ];
 
     return (
         <Swiper
-            modules={[Pagination]}
+            modules={[Pagination, Autoplay]}
+            // autoplay={{
+            //     delay: 12000,
+            //     disableOnInteraction: true,
+            // }}
             spaceBetween={50}
             slidesPerView={1}
             pagination={{clickable: true}}
@@ -64,11 +80,19 @@ const Slides = () => {
                         return (
                             <SwiperSlide key={i}>
                                 <picture>
-                                    <source media="(min-width: 2000px)" srcset={item.imgUltraWide}/>
-                                    <source media="(min-width: 900px)" srcset={item.imgDesktop}/>
-                                    <source media="(min-width: 480px)" srcset={item.imgMobile}/>
-                                    <img src={item.imgAlt} alt="IfItDoesntMatchAnyMedia"/>
+                                    <source media="(min-width: 2000px)" srcSet={item.imgUltraWide}/>
+                                    <source media="(min-width: 900px)" srcSet={item.imgDesktop}/>
+                                    <source media="(min-width: 480px)" srcSet={item.imgMobile}/>
+                                    <img src={item.imgDesktop} alt="Standard"/>
                                 </picture>
+                                <div className={'container'}>
+                                    <div className={'container__wrapper'}>
+                                        <div className={'slide-content'}>
+                                            <h3>{item.title}</h3>
+                                            <p>{item.previewText}</p>
+                                        </div>
+                                    </div>
+                                </div>
                             </SwiperSlide>
                         )
                     }
