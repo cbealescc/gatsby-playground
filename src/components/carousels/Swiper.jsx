e64d507c28a02d0a84a4c07ff7dc8f31135c78de
Conor Beales <connor.beales@collectingcars.com>
2022-06-17 15:42:17 +0100

    Video src now repsonsive

--- src/components/carousels/Swiper.jsx
+++ src/components/carousels/Swiper.jsx
@@ -1,55 +1,33 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 
 // Import Swiper React components
 import {Swiper, SwiperSlide} from 'swiper/react';
-
-// import Swiper core and required modules
 import {Pagination, Autoplay} from 'swiper';
+
+import {BaseUrl, ResponsiveImages} from "./SwiperSlides";
 
 // Import Swiper styles
 import 'swiper/css';
 import 'swiper/css/pagination';
 
-
 const Slides = () => {
-    const baseUrl = 'https://collectingcars.imgix.net/'
 
-    const responsiveImages = [
-        {
-            type: 'image',
-            imgUltraWide: `${baseUrl}012042/IMG-3569.jpg`,
-            imgDesktop: `${baseUrl}012042/30-ww-16.jpg`,
-            imgMobile: `${baseUrl}012042/IMG-3590.jpg`,
-            title: 'Supercar Sunday!',
-            previewText: `If you're a Supercar fan, you'll love this!`,
-            link: '/collection/supercar-sunday',
-            buttonText: 'View Collection'
-        },
-        {
-            type: 'image',
-            imgUltraWide: `${baseUrl}012080/MMP-49.jpg`,
-            imgDesktop: `${baseUrl}012080/MMP-1.jpg`,
-            imgMobile: `${baseUrl}012080/MMP-66.jpg`,
-            title: 'Air-Cooled Sunday!',
-            previewText: `Over 200 air-cooled Porsches.`,
-            link: '/collection/air-cooled-sunday',
-            buttonText: 'View Collection'
-        },
-        {
-            type: 'video',
-            videoUrlMP4: `https://ak.picdn.net/shutterstock/videos/1062622186/preview/stock-footage-aerial-shot-of-electrical-car-driving-on-country-road-at-summer-evening-new-suv-vehicle-moving.mp4`,
-            videoUrlWEBM: `https://ak.picdn.net/shutterstock/videos/1062622186/preview/stock-footage-aerial-shot-of-electrical-car-driving-on-country-road-at-summer-evening-new-suv-vehicle-moving.webm`,
-            title: `Chris Harris' Column`,
-            previewText: `"Working hard and wasting money on machinery you simply do not need is the very essence of loving cars".`,
-            link: '/stories/chris-harris-column-week-2',
-            buttonText: 'Read More'
+    const [isMobile, setIsMobile] = useState(window.innerWidth < 480);
+    const [isUltraWide, setIsUltraWide] = useState(window.innerWidth > 2000);
+
+    useEffect(() => {
+        window.addEventListener('resize', handleWindowResize);
+
+        return () => {
+            window.removeEventListener('resize', handleWindowResize)
         }
-        // {
-        //     type: 'video',
-        //     videoUrlMP4: `https://images-collectingcars-com.s3.eu-west-2.amazonaws.com/editorial/videos/cc-best-offer-explained.mp4`,
-        //     videoUrlWEBM: `https://images-collectingcars-com.s3.eu-west-2.amazonaws.com/editorial/videos/cc-best-offer-explained.mp4`
-        // },
-    ];
+
+    }, [])
+
+    const handleWindowResize = () => {
+        setIsMobile(window.innerWidth < 800);
+        setIsUltraWide(window.innerWidth > 2000);
+    }
 
     return (
         <Swiper
@@ -71,13 +49,19 @@ const Slides = () => {
             onSwiper={(swiper) => console.log(swiper)}
         >
 
-            {responsiveImages.map((item, i) => {
+            {ResponsiveImages.map((item, i) => {
                     if (item.type === 'video') {
                         return (
                             <SwiperSlide key={i}>
                                 <video controls={false} autoPlay={true} muted={true} loop={true}>
-                                    <source src={item.videoUrlMP4} type="video/mp4"/>
-                                    <source src={item.videoUrlWEBM} type="video/webm"/>
+                                    <source
+                                        src={isUltraWide ? item.videoUltraWideMP4 : isMobile ? item.videoMobileMP4 : item.videoDesktopMP4}
+                                        type="video/mp4"
+                                    />
+                                    <source
+                                        src={isUltraWide ? item.videoUltraWideWEBM : isMobile ? item.videoMobileWEBM : item.videoDesktopWEBM}
+                                        type="video/webm"
+                                    />
                                     Your browser does not support videos.
                                 </video>
                                 <div className={'container'}>
---
+++ src/components/carousels/SwiperSlides.jsx
@@ -0,0 +1,38 @@
+export const BaseUrl = 'https://collectingcars.imgix.net/'
+
+export const ResponsiveImages = [
+
+    {
+        type: 'image',
+        imgUltraWide: `${BaseUrl}012042/IMG-3569.jpg`,
+        imgDesktop: `${BaseUrl}012042/30-ww-16.jpg`,
+        imgMobile: `${BaseUrl}012042/IMG-3590.jpg`,
+        title: 'Supercar Sunday!',
+        previewText: `If you're a Supercar fan, you'll love this!`,
+        link: '/collection/supercar-sunday',
+        buttonText: 'View Collection'
+    },
+    {
+        type: 'image',
+        imgUltraWide: `${BaseUrl}012080/MMP-49.jpg`,
+        imgDesktop: `${BaseUrl}012080/MMP-1.jpg`,
+        imgMobile: `${BaseUrl}012080/MMP-66.jpg`,
+        title: 'Air-Cooled Sunday!',
+        previewText: `Over 200 air-cooled Porsches.`,
+        link: '/collection/air-cooled-sunday',
+        buttonText: 'View Collection'
+    },
+    {
+        type: 'video',
+        videoUltraWideMP4: `https://ak.picdn.net/shutterstock/videos/1058654677/preview/stock-footage-bruges-belgium-drone-shot-of-the-cityscape-while-sunset.mp4`,
+        videoUltraWideWEBM: `https://ak.picdn.net/shutterstock/videos/1058654677/preview/stock-footage-bruges-belgium-drone-shot-of-the-cityscape-while-sunset.webm`,
+        videoDesktopMP4: `https://ak.picdn.net/shutterstock/videos/1062622186/preview/stock-footage-aerial-shot-of-electrical-car-driving-on-country-road-at-summer-evening-new-suv-vehicle-moving.mp4`,
+        videoDesktopWEBM: `https://ak.picdn.net/shutterstock/videos/1062622186/preview/stock-footage-aerial-shot-of-electrical-car-driving-on-country-road-at-summer-evening-new-suv-vehicle-moving.webm`,
+        videoMobileMP4: `https://ak.picdn.net/shutterstock/videos/1079752463/preview/stock-footage-funny-cute-pug-dog-do-fitness-workout-at-home-dumbbell-in-the-paw-doing-dumbbells-exercise-dog.mp4`,
+        videoMobileWEBM: `https://ak.picdn.net/shutterstock/videos/1079752463/preview/stock-footage-funny-cute-pug-dog-do-fitness-workout-at-home-dumbbell-in-the-paw-doing-dumbbells-exercise-dog.webm`,
+        title: `Chris Harris' Column`,
+        previewText: `"Working hard and wasting money on machinery you simply do not need is the very essence of loving cars".`,
+        link: '/stories/chris-harris-column-week-2',
+        buttonText: 'Read More'
+    }
+];
