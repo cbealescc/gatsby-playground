99ec43eeb3a6ebc363629c3a4197830cfe71bd90
Connor Beales <connor.beales@collectingcars.com>
2022-09-12 17:39:36 +0100

    Trying to get a map with array of cars object

--- src/pages/experiments/CarList.jsx
+++ src/pages/experiments/CarList.jsx
@@ -1,51 +1,78 @@
-import * as React from "react"
+import React, {useState, useEffect} from "react"
 import {carStats} from "./carsData";
 
 const carMakes = carStats.map(val => val.make);
 
+let {...carDataList} = carStats;
+
 // Returns only cars with make of 'Audi'
 // let audiOnly = carStats.filter(car => car.make === "Audi");
 
-// const changeMake = (e) => {
-//     data['make'] = e.target.id;
-//     console.log(data)
-// }
-
 function CarsList() {
+    const [vehicleList, setVehicleList] = useState([carStats]);
 
-    // Adds the 'FaceLift' property and value to each car
-    carStats.forEach(car => {
-        car['faceLift'] = 'N/A';
-        if (car.year <= 2011) {
-            car['faceLift'] = 'no'
-        } else {
-            car['faceLift'] = 'yes';
-        }
-    })
+    useEffect(() => {
+        setActive();
+        setFacelift();
+    });
+
+    const setFacelift = () => {
+        // Adds the 'FaceLift' property and value to each car
+        carStats.forEach(car => {
+            car['faceLift'] = 'N/A';
+            if (car.year <= 2011) {
+                car['faceLift'] = 'no'
+            } else {
+                car['faceLift'] = 'yes';
+            }
+        })
+    }
+
+    const setActive = () => {
+        carStats.forEach(car => {
+            car['active'] = true;
+        })
+    }
+
+    const filterResults = (e) => {
+        let newResults = [...carStats];
+        const targetId = e.target.id;
+
+        carStats.forEach(car => {
+            car.active = car.make === targetId;
+        })
+    }
+
+    let objectValues = Object.values(carStats);
+
 
     return (
         <>
+
             <ul>
-                {carMakes.map((item, index) => {
-                    return <li key={index}>{item}</li>
-                })}
-            </ul>
+                {/*{*/}
+                {/*    carStats.map((item, i) => (*/}
+                {/*       return(item)*/}
+                {/*    ));*/}
+                {/*}*/}
 
-            <button
-                // onClick={(e) => changeMake(e)}
-                id={'BMW'}
-            >
-                BMW Stats
-            </button>
+                    </ul>
 
-            <button
-                // onClick={(e) => changeMake(e)}
-                id={'Audi'}
-            >
-                Audi Stats
-            </button>
-        </>
-    )
-}
+                    <button
+                    onClick={(e) => filterResults(e)}
+                    id={'BMW'}
+                    >
+                    BMW Stats
+                    </button>
 
-export default CarsList;
+                    <button
+                    onClick={(e) => filterResults(e)}
+                    id={'Audi'}
+                    >
+                    Audi Stats
+                    </button>
+                    </>
+                    )
+                }
+
+                export default CarsList;
